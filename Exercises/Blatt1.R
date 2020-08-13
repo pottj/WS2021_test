10fecf996bbfeaa87eee775ba97500ed52a0046e
Janne <40432007+pottj@users.noreply.github.com>
2020-08-13 11:01:22 +0200

    Update and rename Aufgabe1.R to Blatt1.R

---
+++ Exercises/Blatt1.R
@@ -0,0 +1,21 @@
+# Exercise 1
+
+Im Idealfall als Rmd und dann kann der/die Teilnehmer/in das Skript weiterschreiben. 
+
+## Aufgabe 1
+***
+Grundrechenarten in R ...
+
+```{r chunk1}
+x<-5+4
+x
+```
+
+## Aufgabe 2
+***
+Plotten ...
+
+```{r chunk2}
+summary(cars)
+plot(pressure)
+```
